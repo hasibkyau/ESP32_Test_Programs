61cb6ac34c02a2cb8b588b94c5bd26bfab1ba5ed
Hasibur Rahman <59811661+hasibkyau@users.noreply.github.com>
2022-06-03 07:27:42 +0600

    Add files via upload

---
+++ touchPin/touchPin (2022_05_30 15_51_13 UTC).ino
@@ -0,0 +1,20 @@
+int Touch;
+int dt = 1;
+void setup() {
+  Serial.begin(115200);
+  delay(1000); // give me time to bring up serial monitor
+  Serial.println("ESP32 Touch Test");
+}
+
+void loop() {
+  Touch = touchRead(4);
+  //Serial.println(Toogle);  // get value of Touch 0 pin = GPIO 4
+  if(Touch <= 30){
+    (dt == 1) ? dt = 0: dt = 1;
+    Serial.println(dt);
+   // dt != dt;
+  }
+  delay(200);
+}
+
+
